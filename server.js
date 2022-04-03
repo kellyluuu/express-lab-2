8d5481128cda48435a5008794b70c032589bbaee
KELLY LUU <kellyluuu@gmail.com>
2022-04-02 19:38:51 -0500

    "Tip Calculator express application created."
    "Tip Calculator express application created."

--- server.js
+++ server.js
@@ -5,10 +5,15 @@ app.get("/greeting",(req,res)=>{
     res.send(`Hello Stranger`)
 })
 
-
 app.get("/greeting/:name",(req,res)=>{
     res.send(`${req.params.name}! It's so great to see you!`)
 })
+
+app.get("/tip/:total/:tipPercentage",(req, res)=>{
+    res.send(`Tip Amount = $${parseInt(req.params.total)*parseInt(req.params.tipPercentage)*.01}`)
+})
+
+
 
 
 app.listen(3001,()=>{
